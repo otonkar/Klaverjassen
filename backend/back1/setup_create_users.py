6897a263d5c6f92b13d6242ca68d5079866b945b
otonkar <otonkar@gmail.com>
2020-05-04 16:26:21 +0200

    update django setup

--- backend/back1/setup_create_users.py
+++ backend/back1/setup_create_users.py
@@ -1,6 +1,6 @@
 from my_auth.models import User
 
-# Create a superuser
+Create a superuser
 User.objects.create_superuser(
     username='Admin',
     email='ole.karlsen@gmail.com',
@@ -9,7 +9,34 @@ User.objects.create_superuser(
 # Create test users
 User.objects.create_user(
     username = 'ole',
-    password = 'Tonsberg01!',
+    password = 'Tonsberg01',
+    email = 'ole.karlsen@gmail.com',
+    is_superuser = False,
+    is_active = True,
+    is_staff = True
+)
+
+User.objects.create_user(
+    username = 'Test1',
+    password = 'test1test1',
+    email = 'ole.karlsen@gmail.com',
+    is_superuser = False,
+    is_active = True,
+    is_staff = True
+)
+
+User.objects.create_user(
+    username = 'Test2',
+    password = 'test2test2',
+    email = 'ole.karlsen@gmail.com',
+    is_superuser = False,
+    is_active = True,
+    is_staff = True
+)
+
+User.objects.create_user(
+    username = 'Test3',
+    password = 'test3test3',
     email = 'ole.karlsen@gmail.com',
     is_superuser = False,
     is_active = True,
