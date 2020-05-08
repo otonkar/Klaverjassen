62ba49b273e6ffa48ffafcee07a1c69a08b23495
otonkar <otonkar@gmail.com>
2020-05-08 22:17:37 +0200

    settings

--- backend/back1/back1/settings_prod.py
+++ backend/back1/back1/settings_prod.py
@@ -91,10 +91,21 @@ WSGI_APPLICATION = 'back1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Klaverjas',
+        'USER': 'postgres',
+        'PASSWORD': 'Tonsberg01',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
