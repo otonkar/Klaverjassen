9a9661108a05ceaf580a34cbb9d88f140754d338
root <otonkar@gmail.com>
2020-05-15 15:50:35 +0200

    deploy

--- backend/back1/back1/settings_prod.py
+++ backend/back1/back1/settings_prod.py
@@ -90,23 +90,23 @@ WSGI_APPLICATION = 'back1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'db_klaverjas',
-#         'USER': 'postgres',
-#         'PASSWORD': 'Tonsberg01',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db_klaverjas',
+        'USER': 'postgres',
+        'PASSWORD': 'Tonsberg01',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
