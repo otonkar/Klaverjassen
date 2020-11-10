47c9ca7905486e83b2c038f965fe264291602401
otonkar <otonkar@gmail.com>
2020-11-10 23:42:40 +0100

    Dockerize

--- backend/scripts/run_daphne_prd.sh
+++ backend/scripts/run_daphne_prd.sh
@@ -7,21 +7,18 @@
 # After the error the other commands will not be executed.
 set -e
 
-# load the enviroment variables (defined as: export ENV="development")
-# Note, this will be loaded via the env_file in the docker-compose.
-# source ../backend_dev.env
+######################################################################
+#######  RUN THIS PART ONLY WHEN NO DATABASE HAS BEEN ALREADY CREATED
 
-echo "*** Do the database migrations"
-python manage.py makemigrations my_auth
-python manage.py migrate my_auth
-python manage.py makemigrations klaverjas
-python manage.py migrate klaverjas
-# python manage.py makemigrations channels
-# python manage.py migrate channels
-python manage.py makemigrations appwebsocket
-python manage.py migrate appwebsocket
-python manage.py makemigrations
-python manage.py migrate
+# echo "*** Do the database migrations"
+# python manage.py makemigrations my_auth
+# python manage.py migrate my_auth
+# python manage.py makemigrations klaverjas
+# python manage.py migrate klaverjas
+# python manage.py makemigrations appwebsocket
+# python manage.py migrate appwebsocket
+# python manage.py makemigrations
+# python manage.py migrate
 
 # #####################################################################
 # #### Only run the first time to set base tables in an empty database
@@ -33,7 +30,9 @@ python manage.py migrate
 
 # echo "*** Set the Troef tabel"
 # python manage.py runscript setup_troef.py
-# ####################################################################
+
+#######  END PART
+#####################################################################
 
 # start the daphne server for the backend on port 6000 within the container
 daphne -b 0.0.0.0 -p 6000 back1.asgi:application
--- backend/scripts/run_dev.sh
+++ backend/scripts/run_dev.sh
@@ -7,33 +7,32 @@
 # After the error the other commands will not be executed.
 set -e
 
-# load the enviroment variables (defined as: export ENV="development")
-# Note, this will be loaded via the env_file in the docker-compose.
-# source ../backend_dev.env
+#####################################################################
+######  RUN THIS PART ONLY WHEN NO DATABASE HAS BEEN ALREADY CREATED
 
-# echo "*** Do the database migrations"
-# python manage.py makemigrations my_auth
-# python manage.py migrate my_auth
-# python manage.py makemigrations klaverjas
-# python manage.py migrate klaverjas
-# # python manage.py makemigrations channels
-# # python manage.py migrate channels
-# python manage.py makemigrations appwebsocket
-# python manage.py migrate appwebsocket
-# python manage.py makemigrations
-# python manage.py migrate
+echo "*** Do the database migrations"
+python manage.py makemigrations my_auth
+python manage.py migrate my_auth
+python manage.py makemigrations klaverjas
+python manage.py migrate klaverjas
+python manage.py makemigrations appwebsocket
+python manage.py migrate appwebsocket
+python manage.py makemigrations
+python manage.py migrate
 
-# #####################################################################
-# #### Only run the first time to set base tables in an empty database
-# echo "*** Create superuser and test users"
-# python manage.py runscript setup_create_users.py
+#####################################################################
+#### Only run the first time to set base tables in an empty database
+echo "*** Create superuser and test users"
+python manage.py runscript setup_create_users.py
 
-# echo "*** Set the GameStatus tabel"
-# python manage.py runscript setup_gameStatus.py
+echo "*** Set the GameStatus tabel"
+python manage.py runscript setup_gameStatus.py
 
-# echo "*** Set the Troef tabel"
-# python manage.py runscript setup_troef.py
-# ####################################################################
+echo "*** Set the Troef tabel"
+python manage.py runscript setup_troef.py
+
+######  END PART
+####################################################################
 
 
 # start the development server for the backend on port 5000 within the container
